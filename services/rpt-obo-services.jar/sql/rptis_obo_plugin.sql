98256e459548c53e5eecfa78b8a0ca544dbe49d8
Jessie Zamora <jzamss@gmail.com>
2021-06-04 09:13:23 +0800

    Add getFaasInfo support

--- services/rpt-obo-services.jar/sql/rptis_obo_plugin.sql
+++ services/rpt-obo-services.jar/sql/rptis_obo_plugin.sql
@@ -12,4 +12,26 @@ inner join faas_list fl on mr.objid = fl.rpuid
 inner join rpu br on mr.bldgmaster_objid = br.rpumasterid
 inner join faas_list flb on br.objid = flb.rpuid 
 where flb.objid = $P{faasid}
-and fl.state = 'CURRENT';
+and fl.state = 'CURRENT'
+
+[findFaasInfo]
+select 
+	f.objid,
+	f.state,
+	f.titleno,
+	f.titledate,
+	b.objid as barangay_objid,
+	b.name as barangay_name,
+	rp.cadastrallotno as lotno,
+	rp.blockno,
+	rp.street,
+	pc.code as class_code, 
+	pc.name as class_name
+from faas f 
+inner join realproperty rp on f.realpropertyid = rp.objid 
+inner join rpu r on f.rpuid = r.objid 
+inner join propertyclassification pc on r.classification_objid = pc.objid 
+inner join barangay b on rp.barangayid = b.objid 
+where f.objid = $P{objid}
+
+
